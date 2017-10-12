72e64f571b6cd8bed828027983ebe6d50f5d8f08
Zack Robinson <zwrprofessional@gmail.com>
2017-10-11 22:03:09 -0400

    Added fragments

--- app/src/main/java/com/zackeryrobinson/macdashboardbottomnavigationfragmentssplashcolors/MainActivity.java
+++ app/src/main/java/com/zackeryrobinson/macdashboardbottomnavigationfragmentssplashcolors/MainActivity.java
@@ -1,36 +1,47 @@
 package com.zackeryrobinson.macdashboardbottomnavigationfragmentssplashcolors;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PayFragment.OnFragmentInteractionListener, BenefitsFragment.OnFragmentInteractionListener, TrainingFragment.OnFragmentInteractionListener, MarketingFragment.OnFragmentInteractionListener{
 
-    private TextView mTextMessage;
 
+    private AccountFragment frag;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager;
+            FragmentTransaction fragmentTransaction;
             switch (item.getItemId()) {
                 case R.id.navigation_account:
-                    mTextMessage.setText(R.string.title_account);
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new AccountFragment(), "frag").commit();
                     return true;
                 case R.id.navigation_pay:
-                    mTextMessage.setText(R.string.title_pay);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new PayFragment(), "frag").commit();
+
+
+
                     return true;
                 case R.id.navigation_benefits:
-                    mTextMessage.setText(R.string.title_benefits);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new BenefitsFragment(), "frag").commit();
+
                     return true;
                 case R.id.navigation_training:
-                    mTextMessage.setText(R.string.title_training);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new TrainingFragment(), "frag").commit();
+
                     return true;
                 case R.id.navigation_marketing:
-                    mTextMessage.setText(R.string.title_marketing);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new MarketingFragment(), "frag").commit();
+
                     return true;
             }
             return false;
@@ -38,14 +49,29 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        frag = new AccountFragment();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        fragmentTransaction.replace(R.id.content, frag).commit();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+
+
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
